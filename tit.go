1bf67ce552b8b000f655e2845607c5ad58f1ec20
Mikhael Candra <tjandraraharja99@gmail.com>
2026-04-22 15:43:41 +0700

    make heart pattern

--- tit.go
+++ tit.go
@@ -3,15 +3,29 @@ package main
 import "fmt"
 
 func tit() {
-	n := 5
+	n := 6
 
-	for i := 1; i <= n; i++ {
-		for j := 1; j <= n; j++ {
-			if j == i || j == n-i+1 {
-				fmt.Print("*")
-			} else {
-				fmt.Print(" ")
-			}
+	for i := n/2; i <= n; i += 2 {
+		for j := 1; j < n - i; j += 2 {
+			fmt.Print(" ")
+		}
+		for j := 1; j <= i; j++ {
+			fmt.Print("*")
+		}
+		for j := 1; j < n - i; j++ {
+			fmt.Print(" ")
+		}
+		for j := 1; j <= i; j++ {
+			fmt.Print("*")
+		}
+		fmt.Println()
+	}
+	for i := n; i >= 1; i-- {
+		for j := i; j < n; j++ {
+			fmt.Print(" ")
+		}
+		for j := 1; j <= (i*2) - 1; j++ {
+			fmt.Print("*")
 		}
 		fmt.Println()
 	}
