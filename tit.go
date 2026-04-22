28f3e515a9d42691119807266978608c5f9b2945
Mikhael Candra <tjandraraharja99@gmail.com>
2026-04-22 15:29:32 +0700

    learn make x pattern

--- tit.go
+++ tit.go
@@ -1,5 +1,18 @@
 package main
 
-func tit() string {
-    return "ini dari tit"
+import "fmt"
+
+func tit() {
+	n := 5
+
+	for i := 1; i <= n; i++ {
+		for j := 1; j <= n; j++ {
+			if j == i || j == n-i+1 {
+				fmt.Print("*")
+			} else {
+				fmt.Print(" ")
+			}
+		}
+		fmt.Println()
+	}
 }
